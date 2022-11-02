1b71bdcf0d2438c4ad30dd1670db4598cdc81068
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-02 14:13:55 +0100

    add class Wi

---
+++ DataLayer/Models/Wi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DataLayer.Models
+{
+    public class Wi
+    {
+        public char TConst { get; set; }
+        public string Word { get; set; }
+        public string Field { get; set; }
+        public string Lexeme { get; set; }
+    }
+}
+
