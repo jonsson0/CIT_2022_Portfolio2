e1c21691995dc2090e4b1b0055aa9fb46de6b839
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-02 14:27:04 +0100

    add class Model.Characters

---
+++ DataLayer/Models/Characters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DataLayer.Models
+{
+    public class Characters
+    {
+
+        public char Character_ID { get; set; }
+        public char Person_ID { get; set; }
+        public char Title_ID { get; set; }
+        public string Character { get; set; }
+        
+
+    }
+}
+
