cfae876dd475eb8278d64679e2bffd14b23a7df8
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-02 13:45:24 +0100

    add class title

---
+++ DataLayer/Models/title.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DataLayer.Models
+{
+    public class title
+    {
+        public char titleID { get; set; }
+        public string type { get; set; }
+        public string primarytitle { get; set; }
+        public string originaltitle { get; set; }
+        public string isadult { get; set; }
+        public char startyear { get; set; }
+        public char endyear { get; set; }
+        public int runtimeminutes { get; set; }
+        public string poster { get; set; }
+        public object plot { get; set; }
+        public object MyProperty { get; set; }
+        public double averagerating { get; set; }
+        public int numvotes { get; set; }
+    }
+}
+
