371fe8e95957e6f6346015e975771dfabc4f9175
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-02 14:53:17 +0100

    add class Model.Person

---
+++ DataLayer/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DataLayer.Models
+{
+    public class Person
+    {
+
+        public char PersonId { get; set; }
+        public string Name { get; set; }
+        public char BirthYear { get; set; }
+        public char DeathYear { get; set; }
+    }
+}
+
