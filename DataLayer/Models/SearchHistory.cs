63987956a0c1fae6bb8961b45149abbdbe0236da
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-04 09:08:54 +0100

    add class Model.searchHistory

---
+++ DataLayer/Models/SearchHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace DataLayer.Models
+{
+    public class SearchHistory
+    {
+
+
+        public char HistId { get; set; }
+        public string Username { get; set; }
+        public string HistData { get; set; }
+        public DateTime HistTimeStamp { get; set; }
+    }
+}
+
