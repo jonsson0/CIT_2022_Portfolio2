daf43acadd8ff8ba1a8b6ecfd120646b1591eed0
Mads jonsson <madsjonsson1997@gmail.com>
2022-11-06 14:39:49 +0100

    added getTitle base (empty) added dto for title as main page

--- DataLayer/DataService.cs
+++ DataLayer/DataService.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Models;
 
 namespace DataLayer
 {
     public class DataService : IDataService
     {
+        Title getTItle()
+        {
+            return null;
+        }
+
+       
+
     }
 }
