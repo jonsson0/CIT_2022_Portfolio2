6c0af7c4ab416b282d55fda823ed8d2ffbc08a9b
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-08 18:15:55 +0100

    add getPerson()

--- DataLayer/Models/Persons.cs
+++ DataLayer/Models/Persons.cs
@@ -6,8 +6,8 @@ namespace DataLayer.Models
 
         public string PersonId { get; set; }
         public string Name { get; set; }
-        public int BirthYear { get; set; }
-        public int DeathYear { get; set; }
+        public string? BirthYear { get; set; }
+        public string? DeathYear { get; set; }
     }
 }
 
