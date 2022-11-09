27154774591efb398c3e1913e2dd745a8debe523
Mads jonsson <madsjonsson1997@gmail.com>
2022-11-09 15:15:52 +0100

    outcommented title

--- DataLayer/Models/TitleGenre.cs
+++ DataLayer/Models/TitleGenre.cs
@@ -6,8 +6,12 @@ namespace DataLayer.Models
     public class TitleGenre
     {
         public string TitleId { get; set; }
+     
+        
+        /*
         [JsonIgnore]
         public Title? Title { get; set; }
+        */
 
         public string? Genre { get; set; }
     }
