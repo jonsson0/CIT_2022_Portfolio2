d90b816009fcb00fb9a2e17bb30f55edcc18416a
Monteyi <79196763+Monteyi@users.noreply.github.com>
2022-11-09 10:34:39 +0100

    add person createPerson

--- DataLayer/Models/BookmarkPerson.cs
+++ DataLayer/Models/BookmarkPerson.cs
@@ -10,8 +10,14 @@ namespace DataLayer.Models
     {
         public string UserName { get; set; }
         public User User { get; set; }
+<<<<<<< Updated upstream
         public string PersonName { get; set; }
         public Persons Persons { get; set; }
+=======
+        public User Username { get; set; }
+        public Person Persons { get; set; }
+        public Person Name { get; set; }
+>>>>>>> Stashed changes
         public DateTime Timestamp { get; set; }
     }
 }
