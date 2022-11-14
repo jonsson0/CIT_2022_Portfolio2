28cd5cf0614dd6e1955a2d81a959b0a966534b19
Steen <steenleu@gmail.com>
2022-11-15 00:00:33 +0100

    delete space

--- CIT_2022_Portfolio2/Models/PersonModel.cs
+++ CIT_2022_Portfolio2/Models/PersonModel.cs
@@ -10,9 +10,6 @@ namespace CIT_2022_Portfolio2.Models
         public string? BirthYear { get; set; }
         public string? DeathYear { get; set; }
         public string CoActorPersonsUrl { get; set; }
-
-
-
     }
 }
 
