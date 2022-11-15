20362f9bf5d468404762bc8c40222cde331a6fcd
Steen <steenleu@gmail.com>
2022-11-15 15:37:05 +0100

    add test  not working correct

--- Portfolio2.Tests/DataServiceTests.cs
+++ Portfolio2.Tests/DataServiceTests.cs
@@ -1,11 +1,28 @@
-﻿namespace Portfolio2.Tests
+﻿
+using CIT_2022_Portfolio2.Models;
+using DataLayer.Models;
+
+namespace Portfolio2.Tests
 {
     public class DataServiceTests
     {
         [Fact]
-        public void Test1()
+        public void Person_Object_HasPersonIdNameBirthYearDeathYear()
         {
-
+            var person = new Person();
+            Assert.Null(person.PersonId); // should not be null
+            Assert.Null(person.Name); // should not be null
+            Assert.Null(person.BirthYear);
+            Assert.Null(person.DeathYear);
+        }
+        [Fact]
+        public void PersonModel_Object_HasPersonIdNameBirthYearDeathYear()
+        {
+            var personModel = new PersonModel();
+            Assert.Null(personModel.url);
+            Assert.Null(personModel.Name); // should not be null
+            Assert.Null(personModel.BirthYear);
+            Assert.Null(personModel.DeathYear);
         }
     }
 }
