e061c2bff95d2cdba12c917e5bfed32b0e32d6d8
jonsson0 <madsjonsson1997@gmail.com>
2022-12-04 17:02:00 +0100

    Added a landing page to be able to go to titles and persons

---
+++ CIT_2022_Portfolio2/Controllers/LandingController.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIT_2022_Portfolio2.Controllers
+{
+    [Route("api/Landing")]
+    [ApiController]
+    public class LandingController : Controller
+    {
+        private IDataService _dataService;
+
+        [HttpGet(Name = "Landing")]
+        //[Authorize]
+        public IActionResult getUsers()
+        {
+            List<string> list = new List<string>();
+            list.Add("http://localhost:5001/api/titles");
+            list.Add("http://localhost:5001/api/persons");
+            return Ok(list);
+        }
+
+    }
+}
